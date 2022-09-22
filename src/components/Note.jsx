af1749e8e8b45fb428387c1f95f0e645e5f55517
HabibUrRehmanBhattii <egalienv@gmail.com>
2022-09-22 04:39:22 +0200

    version 1

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,11 +1,21 @@
 import React from "react";
+import DeleteIcon from "@mui/icons-material/DeleteRounded";
 
 function Note(props) {
+
+  // function handleClick(){
+  //   props.onDelete(props.id);
+  // }
+
   return (
     <div className="note">
       <h1>{props.title}</h1>
       <p>{props.content}</p>
-      <button>DELETE</button>
+      <button
+      onClick={() => {
+        props.onDelete(props.id);
+      }}
+      ><DeleteIcon /></button>
     </div>
   );
 }
