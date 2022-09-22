d9a777bab8b8252f01298ed21d614d2b3a394e61
Habib ur rehman Bhatti <109253977+HabibUrRehmanBhattii@users.noreply.github.com>
2022-09-23 01:42:28 +0200

    Updates

---
+++ precache-manifest.f981d7865ec1e0cb5aea42d7e2ff73a1.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "331c2a504da7ba748a07",
+    "url": "/Keeper_clone_v1/static/js/main.331c2a50.chunk.js"
+  },
+  {
+    "revision": "5fbe76f7e6522dd9efda",
+    "url": "/Keeper_clone_v1/static/js/1.5fbe76f7.chunk.js"
+  },
+  {
+    "revision": "ec6a53771f38348a2543",
+    "url": "/Keeper_clone_v1/static/js/runtime~main.ec6a5377.js"
+  },
+  {
+    "revision": "6039af4adf9c578ea6fe27b524d6703b",
+    "url": "/Keeper_clone_v1/index.html"
+  }
+];
